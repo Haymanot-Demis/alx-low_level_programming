40dae2ac550f9eeff3d0507494173c7d8fcf01e3
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-04 21:03:06 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+for (char i = '0'; i <= '8'; i++)
+{
+for (char j = '1'; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (!(i == '8' && j == '9'))
+putchar(',');
+}
+}
+
+return (0);
+}
