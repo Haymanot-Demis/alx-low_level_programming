b826da1608cb2dc64a904f7a6dc23cec6c02687a
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-04 20:23:15 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ *main - Prints lowercase letters using only putchar
+ *
+ *Description: a program that prints the alphabet in lowercase
+ *Return: 0
+*/
+
+int main(void)
+{
+char a = 'a';
+if (a > 'z')
+{
+putchar(a - 32);
+}
+else
+{
+putchar(a);
+}
+
+return(0);
+}
