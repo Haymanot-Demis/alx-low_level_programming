58c539ea77583049752a39a57d6bceb52a372124
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-04 20:05:18 +0300

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 rem = n % 10;
 if (rem == 0)
-printf("Last digit of %d is %d and is zero\n", n, rem);
+printf("Last digit of %d is %d and is 0\n", n, rem);
 else if (rem > 5)
-printf("Last digit of %d is %d and and is greater than 5\n", n, rem);      
+printf("Last digit of %d is %d and is greater than 5\n", n, rem);      
 else if (rem < 6 && rem != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 
