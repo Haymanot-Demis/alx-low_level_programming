78e92714dfd64e91b88f181bd4ee9e26b99d5475
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-04 18:09:20 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,26 @@
 #include<time.h>
 int main(){
     int var;
-	srand(time(0));
+	// srand(time(0));
+	// var = rand() - RAND_MAX / 2;
+    // if(var > 0){
+    //     printf("%d is positive",var);
+    // }else if(var < 0){
+    //     printf("%d is negative",var);
+    // }
+    // else{
+    //     printf("%d is  zero",var);
+    // }
+
+    srand(time(0));
 	var = rand() - RAND_MAX / 2;
-    if(var > 0){
-        printf("%d is positive",var);
-    }else if(var < 0){
-        printf("%d is negative",var);
-    }
-    else{
-        printf("%d is  zero",var);
-    }
+	if (var == 0)
+		printf("%d is zero\n", var);
+	else if (var > 0)
+		printf("%d is positive\n", var);
+	else if (var < 0)
+		printf("%d is negative\n", var);
+	return (0);
 
     return 0;
 }
