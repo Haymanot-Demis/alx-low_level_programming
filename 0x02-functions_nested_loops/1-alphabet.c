113f82d06d48cf8da19dcacb4cd57e75ae92d518
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-07 21:49:50 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
-void print_alphabet(void){
-    
+/**
+ * print_alphabet  - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+for (char i = 'a'; i < 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
+int main()
+{
+print_alphabet();
+return (0);
 }
