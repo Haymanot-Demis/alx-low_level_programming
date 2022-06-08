8ce9421fbf0dd5dd65085b09c89a2a60956b4dfe
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-08 22:34:46 +0300

    3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -26,7 +26,5 @@ largest = c;
 }
 
 return (largest);
+
 }
-
-
-
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,15 +3,9 @@
 
 #include <stdio.h>
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
