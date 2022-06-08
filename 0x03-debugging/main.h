68b64abc44c432961248e39838bc47c45a3614c4
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-08 22:19:08 +0300

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,15 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
 #endif
