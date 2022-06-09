71cf5ae49c24cdee19ba9e760220c4df9b252986
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-09 21:04:53 +0300

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers between 0 to 9 incl.
+ *
+ * Return: Void.
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+}
