051fe833eb425e2d2f7a34d2a9e544fee35a132a
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-06-13 23:00:56 +0300

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy strg to another string
+ * @dest: pointer
+ * @src: poiter
+ * Return: numbers dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (*(src + i) != '\0')
+{
+*(dest + i) = *(src + i);
+i++;
+}
+dest[i] = '\0';
+return (dest);
+    
+}
---
+++ 0x05-pointers_arrays_strings/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+for(i = 0; *(dest + i) != '\0'; i++ ){
+        printf("%c",*(dest + i));
+    }
+    printf("\n");
